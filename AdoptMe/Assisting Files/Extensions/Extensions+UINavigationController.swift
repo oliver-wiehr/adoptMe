297a9cf913da6a12dbf5404aa48a21096e63dee8
Oliver Wiehr <owiehr@apple.com>
2023-01-01 12:20:20 -0800

    Swipe to go back implemented

---
+++ AdoptMe/Assisting Files/Extensions/Extensions+UINavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions+UINavigationController.swift
+//  AdoptMe
+//
+//  Created by Oliver Wiehr on 1/1/23.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
